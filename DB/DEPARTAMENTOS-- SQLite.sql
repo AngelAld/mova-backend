d2889f7436f8c5916edd8f5410a564434295e206
AngelAld <aaaldana50@gmail.com>
2024-10-21 16:47:10 -0500

    detalle propiedad

---
+++ DB/DEPARTAMENTOS-- SQLite.sql
@@ -0,0 +1,51 @@
+-- SQLite
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Amazonas");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Áncash");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Apurímac");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Arequipa");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Ayacucho");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Cajamarca");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Callao");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Cusco");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Huancavelica");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Huánuco");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Ica");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Junín");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("La Libertad");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Lambayeque");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Lima");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Loreto");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Madre de Dios");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Moquegua");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Pasco");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Piura");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Puno");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("San Martín");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Tacna");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Tumbes");
+INSERT INTO Ubicacion_departamento (nombre)
+VALUES ("Ucayali");
--- Ubicacion/models.py
+++ Ubicacion/models.py
@@ -3,16 +3,14 @@ from django.db import models
 
 # Create your models here.
 class Departamento(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
+    nombre = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.nombre
 
 
 class Provincia(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
+    nombre = models.CharField(max_length=100, unique=True)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -20,8 +18,7 @@ class Provincia(models.Model):
 
 
 class Distrito(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
+    nombre = models.CharField(max_length=100, unique=True)
     provincia = models.ForeignKey(Provincia, on_delete=models.CASCADE)
 
     def __str__(self):
